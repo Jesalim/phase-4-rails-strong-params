0af9fad0a1084c0b9a8008dd36312b9f9ba906ba
Jesalim <jessyculture@gmail.com>
2023-03-20 03:38:23 +0300

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,25 +1,33 @@
 class BirdsController < ApplicationController
-
-  # GET /birds
-  def index
-    birds = Bird.all
-    render json: birds
-  end
-
-  # POST /birds
-  def create
-    bird = Bird.create(name: params[:name], species: params[:species])
-    render json: bird, status: :created
-  end
-
-  # GET /birds/:id
-  def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
-  end
-
+  wrap_parameters format: []
 end
+#   # GET /birds
+#   def index
+#     birds = Bird.all
+#     render json: birds
+#   end
+
+#   # POST /birds
+#   def create
+#     bird = Bird.create(bird_params)
+#     render json: bird, status: :created
+#   end
+
+#   private
+#   # all methods below here are private
+
+#   def bird_params
+#     params.permit(:name, :species)
+#   end
+
+#   # GET /birds/:id
+#   def show
+#     bird = Bird.find_by(id: params[:id])
+#     if bird
+#       render json: bird
+#     else
+#       render json: { error: "Bird not found" }, status: :not_found
+#     end
+#   end
+
+# end
